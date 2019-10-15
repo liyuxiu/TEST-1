53a810ef82d3973936ae1b16125e1390869034bf
liyuxiu <liyuxiumail@163.com>
2019-10-15 10:36:01 +0800

    commit update heroes.component.html

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'O_O' },
+  { id: 12, name: 'V_V' },
+  { id: 13, name: 'CVCVVCV' },
+  { id: 14, name: 'AAAAAAAAA' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'hhh' },
+  { id: 20, name: 'Tttt' },
+  { id: 21, name: 'bbbb' },
+  { id: 22, name: 'mmmmmm' },
+  { id: 23, name: 'FUCK' },
+  { id: 24, name: 'ARM' },
+  { id: 25, name: 'BNFM' },
+  { id: 26, name: 'WNFM' }
+];
